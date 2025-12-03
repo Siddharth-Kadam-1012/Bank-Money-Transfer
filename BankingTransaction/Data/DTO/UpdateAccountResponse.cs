535cf7705c7c01a802a53064481acabda1916760
SiddharthKadam <Siddharth.Kadam@IBM.com>
2025-12-03 07:56:22 +0530

    Initial commit

---
+++ BankingTransaction/Data/DTO/UpdateAccountResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingTransaction.Data.DTO
+{
+    public class UpdateAccountResponse
+    {
+       
+        public int id { get; set; }
+        public string FirstName { get; set; }
+
+         public string LastName { get; set; }
+
+         public string Email { get; set; }
+
+        public string message { get; set; }
+    }
+}
